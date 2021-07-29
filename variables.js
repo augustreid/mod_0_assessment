002b571ed04b6b70f6995a0999a1480724d3a71b
August Reid <c.august.reid@gmail.com>
2021-07-29 17:23:30 -0600

    Add variables

---
+++ variables.js
@@ -0,0 +1,17 @@
+var animal = "Percival P. Cat"
+
+var isCuddly = true
+
+animal = "Rufus"
+
+var num8 = 7
+
+var num3 = 15
+
+var quotient = num3 / num8
+
+var favoriteFilms = ["Kiss Kiss Bang Bang", "Lord of the Rings", "Men in Black"]
+
+favoriteFilms.shift();
+
+animal.length
