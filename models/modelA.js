91895b5bc92850aaa03af811a43002b41bd67ac7
August Reid <c.august.reid@gmail.com>
2021-07-29 17:35:27 -0600

    Add Model A

---
+++ models/modelA.js
@@ -0,0 +1,10 @@
+var laptop {
+  name: "Auggie's Laptop",
+  brand: "Apple",
+  ageInMonths: 1,
+  weightInPounds: 4.5,
+  isFast: true,
+  isExpensive: true,
+  thingsItDoes: ["Turing homework", "Slack", "Spotify"],
+  thingsItDoesNotDo: ["dance", "wash dishes", "pay bills"]
+}
