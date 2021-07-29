fbb9f08c1b1b2beb56dc8c4d2ce78a2da1de69a4
August Reid <c.august.reid@gmail.com>
2021-07-29 17:49:17 -0600

    Add model B

---
+++ models/modelB.js
@@ -0,0 +1,10 @@
+var house = {
+  nameOfStreet: "Highway 42",
+  nameOfCity: "Louisville",
+  streetNumber: 2040,
+  ageInYears: 30,
+  isColorPink: false,
+  isCozy: true,
+  treesInYard: ["spruce", "cottonwood", "magnolia"],
+  typesOfOccupants: ["human", "cat", "dog", "mouse", "spider"]
+}
